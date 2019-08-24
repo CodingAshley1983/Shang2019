91a8dd5b7192bcc20d3522709a23088c59aaeb9b
ashley <ashley@abmp.com>
2019-08-24 00:56:32 -0600

    ran build

---
+++ client/build/precache-manifest.d64fbefd9f45cf87f63e61c6708d6b62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "916f1ae3b011849fa3b6",
+    "url": "/static/js/main.916f1ae3.chunk.js"
+  },
+  {
+    "revision": "6fe173fd216dee1d85de",
+    "url": "/static/js/2.6fe173fd.chunk.js"
+  },
+  {
+    "revision": "916f1ae3b011849fa3b6",
+    "url": "/static/css/main.73b0679d.chunk.css"
+  },
+  {
+    "revision": "7b8755e2ae03573cdaabe85f62ec7bc2",
+    "url": "/index.html"
+  }
+];
